8ee6ab7542dab1834388e6effb0cc6bbd370a113
stephengrider <ste.grider@gmail.com>
2018-02-01 15:28:32 -0800

    added image upload, style polish

--- client/src/components/blogs/BlogList.js
+++ client/src/components/blogs/BlogList.js
@@ -7,16 +7,27 @@ class BlogList extends Component {
     this.props.fetchBlogs();
   }
 
+  renderImage({ file }) {
+    if (file) {
+      return (
+        <div className="card-image">
+          <img src={file} />
+        </div>
+      );
+    }
+  }
+
   renderBlogs() {
     return this.props.blogs.reverse().map(blog => {
       return (
-        <div className="card darken-1" key={blog._id}>
-          <div className="card-content">
-            <span className="card-title">{blog.title}</span>
-            <p>{blog.content}</p>
-            <p className="right">
-              Sent On: {new Date(blog.dateSent).toLocaleDateString()}
-            </p>
+        <div className="card darken-1 horizontal" key={blog._id}>
+          {this.renderImage(blog)}
+
+          <div className="card-stacked">
+            <div className="card-content">
+              <span className="card-title">{blog.title}</span>
+              <p>{blog.content}</p>
+            </div>
           </div>
         </div>
       );
--- models/Blog.js
+++ models/Blog.js
@@ -1,10 +1,20 @@
+const keys = require('../config/keys');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const blogSchema = new Schema({
   title: String,
   content: String,
+  file: { type: String, get: filePath },
   _user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
+
+blogSchema.set('toJSON', { getters: true });
+
+function filePath(file) {
+  if (file) {
+    return `${keys.s3BucketUrl}${file}`;
+  }
+}
 
 mongoose.model('blogs', blogSchema);
